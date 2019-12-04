5b2fd918fcf992114bf22b638f9dba66bcc34d49
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-04 14:23:15 -0800

    pull in enemy champ images

--- src/ShowPlayer/index.js
+++ src/ShowPlayer/index.js
@@ -10,7 +10,8 @@ class ShowPlayer extends Component {
         recentMatches: [],
         recentMatchStats: [],
         championsUsed: [],
-        opponents: []
+        opponents: [],
+        opposingChampionsUsed: []
     }
     async componentDidMount(){
         const summonerName = this.props.match.params.summoner
@@ -31,20 +32,30 @@ class ShowPlayer extends Component {
             }
         }
         console.log(champsUsed, "CHAMPS USED")
+        const opponentChampsUsed = []
+        for(let i = 0; i < summoner.opponents.length; i++){
+            for(let j = 0; j < champListNames.length; j++){
+                if(summoner.opponents[i].championId === Number(champList[champListNames[j]].key)){
+                    opponentChampsUsed.push(champListNames[j])
+                }
+            }
+        }
+        console.log(opponentChampsUsed, "OPPOSING CHAMPS USED")
         this.setState({
             level: summoner.summoner.summonerLevel,
             id: summoner.summoner.id,
             recentMatches: summoner.matches,
             recentMatchStats: summoner.stats,
             championsUsed: champsUsed,
-            opponents: summoner.opponents
+            opponents: summoner.opponents,
+            opposingChampionsUsed: opponentChampsUsed
         })
     }
     render(){
         const lastFiveMatches = this.state.recentMatchStats.map((stat, i) => {
             return(
                 <div className="match-stats" key={i}>
-                    <img id="match-history-champs" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${this.state.championsUsed[i]}_0.jpg`}/><br/>
+                    <img className="match-history-champs" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${this.state.championsUsed[i]}_0.jpg`}/><br/>
                     {this.state.championsUsed[i]}<br/>
                     Role: {stat.timeline.role}<br/>
                     Lane: {stat.timeline.lane}<br/>
@@ -62,12 +73,39 @@ class ShowPlayer extends Component {
                 </div>
             )
         })
+        const opponents = this.state.opponents.map((stat, i) => {
+            return(
+                <div className="match-stats" key={i}>
+                    <img className="match-history-champs" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${this.state.opposingChampionsUsed[i]}_0.jpg`}/><br/>
+                    {this.state.opposingChampionsUsed[i]}<br/>
+                    Role: {stat.timeline.role}<br/>
+                    Lane: {stat.timeline.lane}<br/>
+                    Kills: {stat.stats.kills}<br/>
+                    Deaths: {stat.stats.deaths}<br/>
+                    Assists: {stat.stats.assists}<br/>
+                    Gold: {stat.stats.goldEarned}<br/>
+                    {
+                        stat.stats.win === true
+                        ?
+                        "Result: Won"
+                        :
+                        "Result: Loss"
+                    }
+                </div>
+            )
+        })
         return(
-            <div>
-                {this.state.name}<br/>
-                {this.state.level}
-                <h3>Last 5 Matches</h3>
-                {lastFiveMatches}
+            <div id="show-player-container">
+                <div>
+                    {this.state.name}<br/>
+                    {this.state.level}
+                    <h3>Last 5 Matches</h3>
+                    {lastFiveMatches}
+                </div>
+                <div>
+                    <h3>Opponents</h3>
+                    {opponents}
+                </div>
             </div>
         )
     }
