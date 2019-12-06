4305739fbe481c840774e5ece089f393d3686eee
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-06 14:26:42 -0800

    clean app

--- src/App.js
+++ src/App.js
@@ -17,22 +17,6 @@ class App extends Component{
     username: "",
     userId: ""
   }
-  async componentDidMount(){
-    try{
-      const message = await fetch("/api/v1/champions")
-      const messageJson = await message.json()
-      // console.log(messageJson)
-      this.setState({
-        summoner: messageJson
-      })
-      // const matchHistory = await fetch(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${this.state.accountId}?api_key=RGAPI-b6ac37f9-a7d0-44f4-b167-62d30f8b358d`)
-      // const matchHistoryJson = await matchHistory.json()
-      // console.log(matchHistoryJson)
-    }
-    catch(err){
-      console.log(err)
-    }
-  }
 
   render(){
     return (
