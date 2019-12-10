b0aadae8c40e8f9af1e6d94ad57a314ecd2a2853
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-10 09:52:05 -0800

    add styling to render

--- src/ShowUser/index.js
+++ src/ShowUser/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { withRouter, Link} from 'react-router-dom'
+import "./showUser.css"
 
 class User extends Component {
     render(){
 
         const favorites = this.props.favorites.map((favorite, i) => {
             return (
-                <div className="favorite-col" key={i}>
-                    <Link to={favorite.summonerUrl}>{favorite.summonerName}</Link>
-                </div>
+                <Link key={i} to={favorite.summonerUrl} className="favorite-summoner">{favorite.summonerName}</Link>
             )
         })
 
