26675865ecda06dd6263f7539a237eb777ea6584
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-11 09:51:00 -0800

    debug login and register

--- src/Login/index.js
+++ src/Login/index.js
@@ -27,11 +27,11 @@ class Login extends Component {
                 <div id="register-container">
                     <div className="login-header">Register</div>
                     <form onSubmit={this.props.handleRegister}>
-                        <input className="login-input" pattern="\S+" type="text" placeholder="First Name" name="firstName" onChange={this.props.onChange} value={this.props.state.firstName}></input><br/>
-                        <input className="login-input" pattern="\S+" type="text" placeholder="Last Name" name="lastName" onChange={this.props.onChange} value={this.props.state.lastName}></input><br/>
-                        <input className="login-input" pattern="\S+" type="text" placeholder="Username" name="username" onChange={this.props.onChange} value={this.props.state.username}></input><br/>
-                        <input className="login-input" type="text" placeholder="Email" name="email" onChange={this.props.onChange} value={this.props.state.email}></input><br/>
-                        <input className="login-input" type="text" placeholder="Password" name="password" onChange={this.props.onChange} value={this.props.state.password}></input><br/>
+                        <input className="login-input" pattern="\S+" title="No spaces." type="text" placeholder="First Name" name="firstName" onChange={this.props.onChange} value={this.props.state.firstName} required></input><br/>
+                        <input className="login-input" pattern="\S+" title="No spaces." type="text" placeholder="Last Name" name="lastName" onChange={this.props.onChange} value={this.props.state.lastName} required></input><br/>
+                        <input className="login-input" pattern="\S+" title="No spaces." type="text" placeholder="Username" name="username" onChange={this.props.onChange} value={this.props.state.username} required></input><br/>
+                        <input className="login-input" type="text" placeholder="Email" name="email" pattern="\S+" title="No spaces." onChange={this.props.onChange} value={this.props.state.email} required></input><br/>
+                        <input pattern="\S+" title="No spaces." className="login-input" type="password" placeholder="Password" name="password" onChange={this.props.onChange} value={this.props.state.password} required></input><br/>
                         <div id="main-question">Who's your main?</div>
                         <select id="main-selection" name="profileIconUrl" onChange={this.props.onChange}>
                             {champs}
@@ -43,8 +43,8 @@ class Login extends Component {
                 <div id="login-container">
                     <div className="login-header">Login</div>
                     <form onSubmit={this.props.handleLogin}>
-                        <input className="login-input" pattern="\S+" type="text" placeholder="Username" name="loginUsername" onChange={this.props.onChange} value={this.props.state.loginUsername}></input><br/>
-                        <input className="login-input" type="text" placeholder="Password" name="loginPassword" onChange={this.props.onChange} value={this.props.state.loginPassword}></input><br/>
+                        <input className="login-input" type="text" placeholder="Username" name="loginUsername" onChange={this.props.onChange} value={this.props.state.loginUsername} required></input><br/>
+                        <input className="login-input" type="password" placeholder="Password" name="loginPassword" onChange={this.props.onChange} value={this.props.state.loginPassword} required></input><br/>
                         <div className="error-message">{this.props.state.loginErrorMessage}</div>
                         <button className="submit-button" type="submit">Submit</button><br/>
                     </form>
